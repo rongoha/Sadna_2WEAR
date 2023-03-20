091de9d07beab9fd0445ecc389c052b17751d107
stavinyo <stavco28@gmail.com>
2023-03-20 11:56:09 +0200

    סיכום הזמנה
    תיקנתי כפתורים של טרייד והוסף לעגלת קניות
    הוספתי דף חדש של סיכום הזמנה

--- JS/shop-single.js
+++ JS/shop-single.js
@@ -1,14 +1,3 @@
-
-$(document).ready(function(){
-
-    $("#buy").click(function()
-    {
-        $("#buy").hide();
-        $("#addToCart").hide();
-    })
-  });
-  
-
   
  function cartMes(){
 
@@ -57,7 +46,6 @@ function showSlides(n) {
     function openUploadDialog() {
       // Hide the Buy and Add To Cart buttons
       document.getElementById("buy").style.display = "none";
-      document.getElementById("addToCart").style.display = "none";
       // Show the upload form and the Cancel button
       document.getElementById("upload-form").style.display = "block";
       document.getElementById("cancel-upload-btn").style.display = "block";
@@ -66,7 +54,6 @@ function showSlides(n) {
     function closeUploadDialog() {
       // Show the Buy and Add To Cart buttons
       document.getElementById("buy").style.display = "block";
-      document.getElementById("addToCart").style.display = "block";
       // Hide the upload form and the Cancel button
       document.getElementById("upload-form").style.display = "none";
       document.getElementById("cancel-upload-btn").style.display = "none";
